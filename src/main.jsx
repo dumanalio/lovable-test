78ca5de23dade1ce1b7ac21f375d269fd7a75515
Ali <duman.ali8000@gmail.com>
2025-09-07 14:20:00 +0200

    debug: isolate chat input with StandaloneChatTest in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import StandaloneChatTest from './StandaloneChatTest';
 
+// This is a temporary debug setup to isolate the chat input issue.
+// It renders ONLY the standalone test component.
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <StandaloneChatTest />
+  </React.StrictMode>
+);
+
